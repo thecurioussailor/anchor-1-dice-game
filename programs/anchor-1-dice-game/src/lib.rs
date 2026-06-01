d958f19087505e3e5275ddb0d012ce52aed785db
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-06-01 10:45:50 +0530

    anchor init

---
+++ programs/anchor-1-dice-game/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("GWWbCt4NwKodxf3cBoovo9bjdXNjhMnWifKczX8KbkLJ");
+
+#[program]
+pub mod anchor_1_dice_game {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
